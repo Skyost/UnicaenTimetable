df572d61d5c843315b22ebdf824cd5447e8f5fda
Skyost <me@skyost.eu>
2016-10-01 00:50:21 +0200

    New version !
    v0.2 released.

--- app/src/main/java/fr/skyost/timetable/fragments/DefaultFragment.java
+++ app/src/main/java/fr/skyost/timetable/fragments/DefaultFragment.java
@@ -1,5 +1,6 @@
 package fr.skyost.timetable.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,10 +9,15 @@ import android.view.ViewGroup;
 
 import fr.skyost.timetable.R;
 
+import hotchemi.android.rate.AppRate;
+
 public class DefaultFragment extends Fragment {
 
 	@Override
 	public final View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+		final Activity activity = this.getActivity();
+		AppRate.with(activity).setInstallDays(3).showRateDialogIfMeetsConditions(activity);
+
 		return inflater.inflate(R.layout.fragment_main_default, container, false);
 	}
 
